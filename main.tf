f60695b4b5d6ec05ed6fa2b829a1a5d3943d68ee
brow1063 <brow1063@algonquinlive.com>
2025-02-08 13:47:41 -0500

    Update: Added init and outputs for main

--- main.tf
+++ main.tf
@@ -94,3 +94,48 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.labelPrefix}-A05-VM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+
+  admin_username = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  # Use an Ubuntu image from Canonical
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  # Include your SSH public key for secure access
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  # Use the cloud-init config to run your initialization script on first boot
+  custom_data = data.cloudinit_config.init.rendered
+}
+
+output "resource_group_name" {
+  description = "The name of the resource group."
+  value       = azurerm_resource_group.rg.name
+}
+
+output "public_ip_address" {
+  description = "The public IP address of the web server."
+  value       = azurerm_public_ip.public_ip.ip_address
+}
